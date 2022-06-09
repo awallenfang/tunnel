bc1f689716acfb1a640885267ce5b2c30d78c831
Ayk Borstelmann <borstelmann.ayk@gmail.com>
2022-06-09 20:41:00 +0200

    Delete config.hpp

--- include/config.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-
-#define SHADER_ROOT std::string("/home/marvin/sciebo/Grafik/Projekt/tunnel/shaders/")
-#define DATA_ROOT std::string("/home/marvin/sciebo/Grafik/Projekt/tunnel/data/")
