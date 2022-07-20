d894f7de8d8b8211c8822b3798c57037aa142bb0
Ayk Borstelmann <ayk.borstelmann@cronn.de>
2022-07-20 15:24:43 +0200

    Add textures

--- src/tunnel.cpp
+++ src/tunnel.cpp
@@ -7,6 +7,7 @@
 #include <chrono>
 #include <string>
 #include <filesystem>
+#include <stb_image.h>
 
 int WINDOW_WIDTH = 1920;
 int WINDOW_HEIGHT = 1080;
@@ -30,13 +31,13 @@ unsigned int depth_rbo = 0;
 unsigned int framebuffer_handle = 0;
 
 void
-resizeCallback(GLFWwindow* window, int width, int height);
+resizeCallback(GLFWwindow *window, int width, int height);
 
 // called whenever the window gets resized
 void
-resizeCallback(GLFWwindow* window, int width, int height);
+resizeCallback(GLFWwindow *window, int width, int height);
 
-void 
+void
 screenDump(int W, int H, int frame);
 
 void write_frame(int W, int H, int frame);
@@ -50,6 +51,38 @@ create_texture_rgba32f(int width, int height) {
     return handle;
 }
 
+unsigned int
+create_texture_rgba32f(int width, int height, float *data) {
+    unsigned int handle;
+    glCreateTextures(GL_TEXTURE_2D, 1, &handle);
+    glTextureStorage2D(handle, 1, GL_RGBA32F, width, height);
+    glTextureSubImage2D(handle, 0, 0, 0, width, height, GL_RGBA, GL_FLOAT, data);
+
+    return handle;
+}
+
+float *
+load_texture_data(std::string filename, int *width, int *height) {
+    int channels;
+    unsigned char *file_data = stbi_load(filename.c_str(), width, height, &channels, 3);
+
+    int w = *width;
+    int h = *height;
+
+    float *data = new float[4 * w * h];
+    for (int j = 0; j < h; ++j) {
+        for (int i = 0; i < w; ++i) {
+            data[j * w * 4 + i * 4 + 0] = static_cast<float>(file_data[j * w * 3 + i * 3 + 0]) / 255;
+            data[j * w * 4 + i * 4 + 1] = static_cast<float>(file_data[j * w * 3 + i * 3 + 1]) / 255;
+            data[j * w * 4 + i * 4 + 2] = static_cast<float>(file_data[j * w * 3 + i * 3 + 2]) / 255;
+            data[j * w * 4 + i * 4 + 3] = 1.f;
+        }
+    }
+
+    delete[] file_data;
+
+    return data;
+}
 
 void build_framebuffer(int width, int height) {
     if (framebuffer_tex) {
@@ -71,19 +104,19 @@ void build_framebuffer(int width, int height) {
     glCreateFramebuffers(1, &fbo);
     glNamedFramebufferTexture(fbo, GL_COLOR_ATTACHMENT0, framebuffer_tex, 0);
     glNamedFramebufferRenderbuffer(fbo, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depth_rbo);
-    if(glCheckNamedFramebufferStatus(fbo, GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+    if (glCheckNamedFramebufferStatus(fbo, GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
         printf("Incomplete FBO!");
         std::terminate();
     }
 }
 
 int
-main(int, char* argv[]) {
-    GLFWwindow* window = initOpenGL(WINDOW_WIDTH, WINDOW_HEIGHT, argv[0]);
+main(int, char *argv[]) {
+    GLFWwindow *window = initOpenGL(WINDOW_WIDTH, WINDOW_HEIGHT, argv[0]);
     glfwSetFramebufferSizeCallback(window, resizeCallback);
 
     std::string vertexShaderName = "tunnel.vert";
-    std::string fragmentShaderName = "voxel_trace.frag";
+    std::string fragmentShaderName = "tunnel_gem.frag";
 
     if (const char *shaderOverride = std::getenv("SHADER_OVERWRITE")) {
         fragmentShaderName = std::string(shaderOverride) + ".frag";
@@ -104,11 +137,11 @@ main(int, char* argv[]) {
     glDeleteShader(fragmentShader);
     glDeleteShader(vertexShader);
 
-	// Define uniform variables
+    // Define uniform variables
     glUseProgram(shaderProgram);
     int res = glGetUniformLocation(shaderProgram, "uRes");
     int time = glGetUniformLocation(shaderProgram, "uTime");
-    //int tex_loc = glGetUniformLocation(shaderProgram, "tex");
+    int tex_loc = glGetUniformLocation(shaderProgram, "tex");
     int seed = glGetUniformLocation(shaderProgram, "init_seed");
     int sample_amt = glGetUniformLocation(shaderProgram, "samples");
     int sample_num = glGetUniformLocation(shaderProgram, "sample_number");
@@ -118,14 +151,14 @@ main(int, char* argv[]) {
 
     // vertex data
     float vertices[] = {
-        -1.0f, -1.0f, 0.0f,
-         1.f, -1.f, 0.0f,
-         -1.f,1.f,0.f,
-         1.0f,  1.f, 0.0f
+            -1.0f, -1.0f, 0.0f,
+            1.f, -1.f, 0.0f,
+            -1.f, 1.f, 0.f,
+            1.0f, 1.f, 0.0f
     };
 
     unsigned int indices[] = {
-        0, 1, 2, 1, 2, 3
+            0, 1, 2, 1, 2, 3
     };
 
     unsigned int VAO;
@@ -134,15 +167,25 @@ main(int, char* argv[]) {
 
     unsigned int VBO = makeBuffer(GL_ARRAY_BUFFER, GL_STATIC_DRAW, sizeof(vertices), vertices);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
 
     unsigned int IBO = makeBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, sizeof(indices), indices);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 
+    int image_width, image_height;
+    float *image_tex_data = load_texture_data(DATA_ROOT + "rustymetal.jpg", &image_width, &image_height);
+    unsigned int image_tex = create_texture_rgba32f(image_width, image_height, image_tex_data);
+    glBindTextureUnit(0, image_tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    delete[] image_tex_data;
+
     build_framebuffer(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-	// Define shader files to check for real-time recompiling
+    // Define shader files to check for real-time recompiling
     const auto vs = "../shaders/" + vertexShaderName;
     const auto fs = "../shaders/" + fragmentShaderName;
 
@@ -157,10 +200,10 @@ main(int, char* argv[]) {
 
     for (int i = 0; i < light_amt; i++) {
         int rando = rand();
-        light_positions.push_back(glm::vec3(cos(rando) * 5., abs(sin(rando))* 5., 0.2f * i - 0.2f));
+        light_positions.push_back(glm::vec3(cos(rando) * 5., abs(sin(rando)) * 5., 0.2f * i - 0.2f));
     }
 
-    
+
     for (int i = 0; i < light_amt; i++) {
         light_colors.push_back((rand() % 4) + 3);
     }
@@ -174,10 +217,10 @@ main(int, char* argv[]) {
             vertexShader = compileShader(vertexShaderName.c_str(), GL_VERTEX_SHADER);
             fragmentShader = compileShader(fragmentShaderName.c_str(), GL_FRAGMENT_SHADER);
             shaderProgram = linkProgram(vertexShader, fragmentShader);
-            
+
             res = glGetUniformLocation(shaderProgram, "uRes");
             time = glGetUniformLocation(shaderProgram, "uTime");
-            //tex_loc = glGetUniformLocation(shaderProgram, "tex");
+            tex_loc = glGetUniformLocation(shaderProgram, "tex");
             seed = glGetUniformLocation(shaderProgram, "init_seed");
             sample_amt = glGetUniformLocation(shaderProgram, "samples");
             sample_num = glGetUniformLocation(shaderProgram, "sample_number");
@@ -194,39 +237,34 @@ main(int, char* argv[]) {
         // and fill screen with it (therefore clearing the window)
         glClear(GL_COLOR_BUFFER_BIT);
 
-        for (int i = 0; i<samples; i++) {
-            // for (int x = 0; x < GRID_X; x++) {
-            //     for (int y = 0; y < GRID_Y; y++) {
-                    // render something...
-                    // Update moving lights
-                    
-                    glUseProgram(shaderProgram);
+        for (int i = 0; i < samples; i++) {
 
-                    glEnable(GL_BLEND);
-                    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  
+            glUseProgram(shaderProgram);
 
-                    glUniform1i(sample_amt, samples);
-                    glUniform1i(sample_num, i);
-                    glUniform1f(time, getTimeDelta(frame));
-                    glUniform1i(seed, rand());
-                    glUniform2ui(res, WINDOW_WIDTH, WINDOW_HEIGHT);
-                    glUniform3fv(light_pos, light_amt, glm::value_ptr(light_positions[0]));
-                    glUniform1iv(light_col, light_amt, &light_colors[0]);
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-                    glBindVertexArray(VAO);
-                    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
-                    // swap buffers with window == show rendered content
-                    
-            //     }
-            // }
+            glUniform1i(sample_amt, samples);
+            glUniform1i(sample_num, i);
+            glUniform1f(time, getTimeDelta(frame));
+            glUniform1i(seed, rand());
+            glUniform1i(tex_loc, 0);
+            glUniform2ui(res, WINDOW_WIDTH, WINDOW_HEIGHT);
+            glUniform3fv(light_pos, light_amt, glm::value_ptr(light_positions[0]));
+            glUniform1iv(light_col, light_amt, &light_colors[0]);
+
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *) 0);
+            // swap buffers with window == show rendered content
+
             glfwSwapBuffers(window);
         }
-        
-        
+
+
         // process window events
         glfwPollEvents();
 
-        if (shouldScreenDump){
+        if (shouldScreenDump) {
             screenDump(WINDOW_WIDTH, WINDOW_HEIGHT, frame);
         }
         if (frame == 600) {
@@ -234,13 +272,12 @@ main(int, char* argv[]) {
         }
         frame++;
     }
-    
+
     glDeleteFramebuffers(1, &fbo);
     glfwTerminate();
 }
 
-void resizeCallback(GLFWwindow*, int width, int height)
-{
+void resizeCallback(GLFWwindow *, int width, int height) {
     // set new width and height as viewport size
     glViewport(0, 0, width, height);
     WINDOW_WIDTH = width;
@@ -250,7 +287,7 @@ void resizeCallback(GLFWwindow*, int width, int height)
 float getTimeDelta(int frame) {
     // auto now = std::chrono::system_clock::now();
     // return static_cast<float>((std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time).count() % 500000) / 1000.f);
-    return (float)frame / FPS;
+    return (float) frame / FPS;
 }
 
 void screenDump(int W, int H, int frame) {
